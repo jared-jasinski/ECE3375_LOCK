92b4a96b3d8c5b7bcbb173ce0175424b3693f86b
Matt <mattliuusa@hotmail.com>
2021-04-01 22:10:13 -0400

    fix declarations

--- headers.h
+++ headers.h
@@ -1,39 +1,32 @@
 #include "address_map_arm.h"
-extern int passwordInput;
-extern int password;
-extern int nInput;
-extern int nSet;
-extern int lookUpTable[10];
-extern int time;
-extern int timeSec;
-extern int timeMin;
+extern volatile int passwordInput;
+extern volatile int password;
+extern volatile int nInput;
+extern volatile int nSet;
+extern volatile int lookUpTable[10];
+extern volatile int time;
+extern volatile int timeSec;
+extern volatile int timeMin;
 
-extern volatile int *HEX_SEC_HUND = (int *)HEX3_HEX0_BASE; //hex 0-3
-extern volatile int *HEX_MINS = (int *)HEX5_HEX4_BASE;     //hex 4-5
-extern volatile int *PUSH_BUTTON = (int *)KEY_BASE;
+extern volatile int *HEX_SEC_HUND; //hex 0-3
+extern volatile int *HEX_MINS;     //hex 4-5
+extern volatile int *PUSH_BUTTON;
 
 //Addresses for timer controls
-extern volatile int *TIMER = (int *)TIMER_BASE;
-extern volatile int *TIMER_CONTROL = (int *)(TIMER_BASE + 0x4);
-extern volatile int *TIMER_LVL = (int *)(TIMER_BASE + 0x8);
-extern volatile int *TIMER_LVH = (int *)(TIMER_BASE + 0xc);
+extern volatile int *TIMER;
+extern volatile int *TIMER_CONTROL;
+extern volatile int *TIMER_LVL;
+extern volatile int *TIMER_LVH;
 
 //address for switch bank
-extern volatile int *SWITCH_BANK = (int *)SW_BASE;
+extern volatile int *SWITCH_BANK;
 
-extern int passwordInput;
-extern int password;
-extern int nInput = 32;
-extern int nSet = 32;
-
-extern int lookUpTable[10];
-
-// prototyping function calls 
+// prototyping function calls
 void ChangePassword();
-int CheckPassword(int );
+int CheckPassword(int);
 void ClearPassword();
-void SetPassword(int , int );
-void InputPassword(int , int );
+void SetPassword(int, int);
+void InputPassword(int, int);
 int readButtons();
 void TimerCountDown();
 void InitTimer();
