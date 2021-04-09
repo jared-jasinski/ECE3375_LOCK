99c3ffac3a26fba77ee8ad571bda99802b18cfda
Matt <mattliuusa@hotmail.com>
2021-04-08 21:18:26 -0400

    all functions passed testing

--- Display.c
+++ Display.c
@@ -3,12 +3,29 @@
 volatile int *hex0 = (int *)HEX3_HEX0_BASE;
 volatile int *hex1 = (int *)HEX5_HEX4_BASE;
 
+int GetPasswordInput() {
+    return passwordInput;
+}
+
+void ClearDisplay() {
+    /**
+     * clears 8 hexdisplays to show nothing
+     */
+    *hex0 = 0x00000000;
+    *hex1 = 0x00000000;
+}
+
 void Display(int value)
 {
+    /**
+     * displays up to an 8-digit value;
+     * assumes "value" is in base10
+     */
+
     volatile int i;
     volatile int a = 0x0;
     volatile int b = 0x0;
-    for (i = 5; i >= 0; i--)
+    for (i = 7; i >= 0; i--)
     {
         int digit = (value % Exponent(10, i + 1)) / (Exponent(10, i));
 
@@ -29,6 +46,10 @@ void Display(int value)
 }
 
 void UpdateDisplay(int digit) {
+    /**
+     * takes a single digit and pushes to the right of the display;
+     * preserving previously inputted digits
+     */
     passwordInput *= 10;
     passwordInput += digit;
 }
@@ -41,24 +62,34 @@ void DisplaySetPass(void)
 
 void DisplayWrongPass(void)
 {
-    int i = 0;
-
-    for (i = 0; i < 10; i++)
+    volatile int j;
+    for (j = 0; j < 5; j++)
     {
 
         *hex1 = 0xFFFFFFFF; //all on
-        *hex1 = 0xFFFFFFFF;
-        FlashDelay();
-        while (!DelayChecker())
-            ;
-        StopTimer();
+        *hex0 = 0xFFFFFFFF;
+        
+        // temp solution
+        volatile int i;
+        for (i = 0; i < 200000; i ++) ;
+
+        // TODO: fix timer
+        // FlashDelay();
+        // while (!DelayChecker())
+        //     ;
+        // StopTimer();
 
         *hex1 = 0x00000000; //all off
         *hex0 = 0x00000000;
-        FlashDelay();
-        while (!DelayChecker())
-            ;
-        StopTimer();
+
+        // temp solution
+        for (i = 0; i < 200000; i ++) ;
+
+        // TODO: fix timer
+        // FlashDelay();
+        // while (!DelayChecker())
+        //     ;
+        // StopTimer();
     }
 }
 
@@ -69,10 +100,15 @@ void AccessGranted(void)
     *hex0 = 0x39796D6D;
     *hex1 = 0x00007739;
 
-    HalfSecondDelay();
-    while (!DelayChecker())
-        ;
-    StopTimer();
+    // TODO: fix timer 
+    // HalfSecondDelay();
+    // while (!DelayChecker())
+    //     ;
+    // StopTimer();
+
+    // temp solution
+    volatile int i;
+    for (i = 0; i < 700000; i ++) ;
 
     //GRANTED
     *hex0 = 0x5478795E;
